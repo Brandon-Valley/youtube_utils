67e870f6049f1a80858c5725a315d667c4cdd25c
Brandon Valley <bavvh8@mst.edu>
2019-10-28 01:40:57 -0500

    dl_audio_only works

---
+++ yt_dl_utils.py
@@ -0,0 +1,25 @@
+from __future__ import unicode_literals
+import youtube_dl
+
+
+
+
+def dl_audio_only(yt_url, out_path = None):
+    ydl_opts = {
+        'format': 'bestaudio/best',
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': '192',
+        }],
+    }
+    
+    if out_path != None:
+        ydl_opts['outtmpl'] = out_path
+
+    
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([yt_url])
+        
+
+# dl_audio_only('https://www.youtube.com/watch?v=hWTFG3J1CP8', 'test2.mp3')
