871384fcc3644fa4f582bd2317cec4ee8045523a
Valley (US), Brandon A <brandon.a.valley@boeing.com>
2020-03-10 12:25:42 -0500

    cleaned up and added dict dl

--- yt_dl_utils.py
+++ yt_dl_utils.py
@@ -26,6 +26,7 @@ def dl_audio_only(yt_url, out_path = None):
         
         
 # downloads yt vid at highest resolution
+# currently will not overwrite existing file, or even throw an error if there is one
 def download_youtube_vid(videourl, vid_save_path):
 #     print(' in dl utils downloading yt vid to : ', path + '/' + save_title + '.mp4')#`````````````````````````````````````````````````
     cmd = 'youtube-dl -f best "' + videourl + '" -o ' + vid_save_path
@@ -44,11 +45,37 @@ def download_youtube_vid(videourl, vid_save_path):
 #     print('in dl_utils, renameing...')#`````````````````````````````````````````````````````````````````````````````
 #     os.rename(newest_file_path, path + '//' + save_title + '.mp4')
 # downloadYouTube('https://www.youtube.com/watch?v=zNyYDHCg06c', './videos/FindingNemo1')
-        
-        
 
-# dl_audio_only('https://www.youtube.com/watch?v=hWTFG3J1CP8', 'test2.wav')
-download_youtube_vid("https://www.youtube.com/watch?v=NaYg00lBKr4", 'test.mp4')
+                                                                                                                                               
+def download_youtube_vids_from_vid_url_dest_path_d(vid_url_dest_path_d):                                                                       
+    for vid_url, dest_path in vid_url_dest_path_d.items():                                                                                     
+        download_youtube_vid(vid_url, dest_path)                                                                                               
+                                                                                                                                               
+                                                                                                                                               
+        
+if __name__ == "__main__":
+    
+
+
+    vids_dir_path = "C:\\Users\\mt204e\\Documents\\projects\\jira_best_practices\\videos\\jira_reports_tutorial_vids"
+    
+#     save_path = "C:\\Users\\mt204e\\Documents\\projects\\jira_best_practices\\videos\\Jira_Reports_Tutorial__Burndown_Charts.mp4"
+#      
+#     # dl_audio_only('https://www.youtube.com/watch?v=hWTFG3J1CP8', 'test2.wav')
+#     download_youtube_vid("https://www.youtube.com/watch?v=hH-q7rimHPY", vids_dir_path + '\\Epic_Report.mp4')
+ 
+    vid_url_dest_path_d = {
+                            'https://www.youtube.com/watch?v=gTOKqz7oW3g' : vids_dir_path + '\\Sprint_Report.mp4',
+#                             'https://www.youtube.com/watch?v=hH-q7rimHPY&list=PLaD4FvsFdarTnxd7YhUNbQceNEV7I_1Wo&index=3' : vids_dir_path + '\\Epic_Report.mp4',
+                            'https://www.youtube.com/watch?v=-h7YlVBnyKM' : vids_dir_path + '\\Velocity_Chart.mp4',
+                            'https://www.youtube.com/watch?v=Z8hM8tr4OAM' : vids_dir_path + '\\Control_Chart.mp4',
+                            'https://www.youtube.com/watch?v=CtVkZDy0ve8' : vids_dir_path + '\\Release_Burndown.mp4',
+                            'https://www.youtube.com/watch?v=U0xht5M_49A' : vids_dir_path + '\\Version_Report.mp4',
+                            'https://www.youtube.com/watch?v=9yYOaN68T-4' : vids_dir_path + '\\Average_Age_Report.mp4',
+ 
+                           }
+     
+    download_youtube_vids_from_vid_url_dest_path_d(vid_url_dest_path_d)
 
 
 
