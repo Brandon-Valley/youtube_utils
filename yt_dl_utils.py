4a10afcf254613d1fffb2b804d22527ea5260406
Brandon Valley <bavvh8@mst.edu>
2022-12-18 08:55:18 -0600

    whitespace

--- yt_dl_utils.py
+++ yt_dl_utils.py
@@ -3,7 +3,7 @@ import os
 from pathlib import Path
 import youtube_dl
 import subprocess
-# 
+#
 # https://www.youtube.com/channel/UCxfpjrP56c2ADNXK9RQ1zAg
 # https://www.youtube.com/channel/UCB7Fk050iWZ2LBfgi9tro3A/videos
 # https://www.youtube.com/channel/UCjXbR88mPHHcuBLHBGKmrjw
@@ -31,8 +31,8 @@ def dl_audio_only(yt_url, out_path = None):
 
     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
         ydl.download([yt_url])
-        
-        
+
+
 # downloads yt vid at highest resolution
 # currently will not overwrite existing file, or even throw an error if there is one
 def download_youtube_vid(videourl, vid_save_path):
@@ -40,13 +40,13 @@ def download_youtube_vid(videourl, vid_save_path):
     cmd = 'youtube-dl -f best "' + videourl + '" -o ' + vid_save_path
 #     print('cmd: ', cmd)#````````````````````````````````````````````````````````````````````````````````````
     subprocess.call(cmd, shell=True)
- 
+
 #     yt = YouTube(videourl)
 #     yt = yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first()
 #     if not os.path.exists(path):
 #         os.makedirs(path)
 #     yt.download(path)
-#      
+#
 #     print('in dl_utils, finding newest file_path...')#`````````````````````````````````````````````````````````````````````````````
 #     # rename saved video
 #     newest_file_path = file_system_utils.get_newest_file_path(path)
@@ -54,12 +54,12 @@ def download_youtube_vid(videourl, vid_save_path):
 #     os.rename(newest_file_path, path + '//' + save_title + '.mp4')
 # downloadYouTube('https://www.youtube.com/watch?v=zNyYDHCg06c', './videos/FindingNemo1')
 
-                                                                                                                                               
-def download_youtube_vids_from_vid_url_dest_path_d(vid_url_dest_path_d):                                                                       
-    for vid_url, dest_path in vid_url_dest_path_d.items():                                                                                     
-        download_youtube_vid(vid_url, dest_path)                                                                                               
-                                                                                                                                               
-# Could improve with threading                                                                                                                                               
+
+def download_youtube_vids_from_vid_url_dest_path_d(vid_url_dest_path_d):
+    for vid_url, dest_path in vid_url_dest_path_d.items():
+        download_youtube_vid(vid_url, dest_path)
+
+# Could improve with threading
 def dl_all_videos_in_playlist(playlist_url, out_dir_path, replace_spaces_with = "_"):
     # https://www.codegrepper.com/tpc/python+download+youtube+playlist
     p = Playlist(playlist_url)
@@ -94,12 +94,12 @@ if __name__ == "__main__":
 
 
 #     vids_dir_path = "C:\\Users\\mt204e\\Documents\\projects\\jira_best_practices\\videos\\jira_reports_tutorial_vids"
-    
+
 # #     save_path = "C:\\Users\\mt204e\\Documents\\projects\\jira_best_practices\\videos\\Jira_Reports_Tutorial__Burndown_Charts.mp4"
-# #      
+# #
 # #     # dl_audio_only('https://www.youtube.com/watch?v=hWTFG3J1CP8', 'test2.wav')
 # #     download_youtube_vid("https://www.youtube.com/watch?v=hH-q7rimHPY", vids_dir_path + '\\Epic_Report.mp4')
- 
+
 #     vid_url_dest_path_d = {
 #                             'https://www.youtube.com/watch?v=gTOKqz7oW3g' : vids_dir_path + '\\Sprint_Report.mp4',
 # #                             'https://www.youtube.com/watch?v=hH-q7rimHPY&list=PLaD4FvsFdarTnxd7YhUNbQceNEV7I_1Wo&index=3' : vids_dir_path + '\\Epic_Report.mp4',
@@ -108,9 +108,9 @@ if __name__ == "__main__":
 #                             'https://www.youtube.com/watch?v=CtVkZDy0ve8' : vids_dir_path + '\\Release_Burndown.mp4',
 #                             'https://www.youtube.com/watch?v=U0xht5M_49A' : vids_dir_path + '\\Version_Report.mp4',
 #                             'https://www.youtube.com/watch?v=9yYOaN68T-4' : vids_dir_path + '\\Average_Age_Report.mp4',
- 
+
 #                            }
-     
+
 #     download_youtube_vids_from_vid_url_dest_path_d(vid_url_dest_path_d)
 
 
