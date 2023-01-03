ca19de76d9b3f224e58b60197d3ee62450af594f
Brandon Valley <bavvh8@mst.edu>
2023-01-02 18:35:19 -0600

    skipping dolphin

--- README.md
+++ README.md
@@ -4,7 +4,10 @@ good diagram for ttml positioning - https://www.speechpad.com/captions/ttml
 
 Great 3 short vid test playlist: https://www.youtube.com/playlist?list=PLfAIhxRGcgam-4wROzza_wfzdHoBJgj2J
 
-has 3 diff en .ttml and / in title name - https://www.youtube.com/watch?v=yPM77NPZyJo&list=PLJBo3iyb1U0eNNN4Dij3N-d0rCJpMyAKQ&index=26
+
+# Good examples
+- Brian 9 - has 3 diff en .ttml and / in title name - https://www.youtube.com/watch?v=yPM77NPZyJo&list=PLJBo3iyb1U0eNNN4Dij3N-d0rCJpMyAKQ&index=26
+- dolphin - 
 # FG Clip channels
  - TBS - https://www.youtube.com/playlist?list=PLJBo3iyb1U0eNNN4Dij3N-d0rCJpMyAKQ
  - https://www.youtube.com/channel/UCB7Fk050iWZ2LBfgi9tro3A/videos
--- yt_dl_utils.py
+++ yt_dl_utils.py
@@ -8,7 +8,12 @@ from pytube import Playlist
 from pytube import YouTube
 
 from sms.logger import txt_logger
-from sms.file_system_utils import file_system_utils as fsu
+from sms.file_system_utils import file_system_utils as fsu\
+
+
+class No_Valid_Subs_Exception(Exception):
+    "en-en makes me sad"
+    pass
 
 
 ####################################################################################################
@@ -16,7 +21,7 @@ from sms.file_system_utils import file_system_utils as fsu
 ####################################################################################################
 def _get_path_safe_str(in_str, replace_spaces_with):
     """ # Replace any special chars that can't be in path with '_' """
-    path_safe_str = in_str.translate({ord(c): "_" for c in ":*<>?|`"})
+    path_safe_str = in_str.translate({ord(c): "_" for c in "/:*<>?|`"})
 
     if replace_spaces_with != None:
         path_safe_str = path_safe_str.replace(" ", replace_spaces_with)
@@ -49,7 +54,11 @@ def get_lone_ext_file_path_in_dir(dir_path, ext):
         file_path = file_path_l[0].__str__()
         return file_path
     else:
-        raise Exception(f"Error: There is more than 1 file with `{ext=}` in dir: {dir_path}, {file_path_l=}")
+        # raise Exception(f"Error: There is more than 1 file with `{ext=}` in dir: {dir_path}, {file_path_l=}")
+        # LATER REALLY SHOULD REMOVE this workaround, should list subs and dl only the best!
+        print("WARNING-WORK-AROUND - There is more than 1 file with `{ext=}` in dir: {dir_path}, {file_path_l=}")
+        file_path = file_path_l[0].__str__()
+        return file_path
 
 
 ####################################################################################################
@@ -114,7 +123,8 @@ def re_time_subs_for_separate_sub_yt_playlist_dl_dir(in_dir_path):
     vid_sub_dir_path_l = fsu.get_dir_content_l(in_dir_path, "dir")
 
     for vid_sub_dir_path in vid_sub_dir_path_l:
-        sub_file_path_l = list(Path(vid_sub_dir_path).glob("*en.ttml"))
+        # sub_file_path_l = list(Path(vid_sub_dir_path).glob("*en.ttml"))
+        sub_file_path_l = list(Path(vid_sub_dir_path).glob("*.ttml"))
 
         if len(sub_file_path_l) == 0:
             print("no subs in ", vid_sub_dir_path)
@@ -152,6 +162,7 @@ def dl_audio_only(yt_url, out_path = None):
 ####################################################################################################
 # LATER should add option to get real subs if exist
 def dl_yt_vid_and_sub__as__mp4_and_sub__w_vid_title(vid_url, out_parent_dir_path, replace_spaces_with = "_"):
+    fsu.delete_if_exists(out_parent_dir_path)
     Path(out_parent_dir_path).mkdir(parents=True, exist_ok=True)
 
     vid = YouTube(vid_url)
@@ -159,38 +170,86 @@ def dl_yt_vid_and_sub__as__mp4_and_sub__w_vid_title(vid_url, out_parent_dir_path
     out_template = os.path.join(out_parent_dir_path, path_safe_vid_title) + ".%(ext)s"
 
     # DL with separate mp4 and srt sub file
-    # cmd = f'yt-dlp -f bestvideo[ext=mp4]+bestaudio[ext=ttml]/best[ext=mp4]/best --write-auto-subs --sub-lang "en.*" --sub-format ttml --no-playlist -o "{out_template}" {vid.watch_url}'
-    cmd = f'yt-dlp -f bestvideo[ext=mp4]+bestaudio[ext=ttml]/best[ext=mp4]/best --write-auto-subs --sub-lang ".en.*" --sub-format ttml --no-playlist -o "{out_template}" {vid.watch_url}'
+    cmd = f'yt-dlp -f bestvideo[ext=mp4]+bestaudio[ext=ttml]/best[ext=mp4]/best --write-auto-subs --sub-lang "en.*" --sub-format ttml --no-playlist -o "{out_template}" {vid.watch_url}'
+    # cmd = f'yt-dlp -f bestvideo[ext=mp4]+bestaudio[ext=ttml]/best[ext=mp4]/best --write-auto-subs --sub-lang en --sub-format ttml --no-playlist -o "{out_template}" {vid.watch_url}'
+    # cmd = f'yt-dlp -f bestvideo[ext=mp4]+bestaudio[ext=ttml]/best[ext=mp4]/best --write-auto-subs --sub-lang .en.* --sub-format ttml --no-playlist -o "{out_template}" {vid.watch_url}'
+    # cmd = f'yt-dlp -f bestvideo[ext=mp4]+bestaudio[ext=ttml]/best[ext=mp4]/best --write-auto-subs --sub-lang en,en-us,en_us,en-US,en_US --sub-format ttml --no-playlist -o "{out_template}" {vid.watch_url}'
     print(f"Running cmd: {cmd}...")
     subprocess.call(cmd, shell = True)
 
+    # Delete all but t...
+    _delete_all_but_best_sub_file_in_dir(out_parent_dir_path, ".ttml")
+
     # Return path to downloaded files
     out_mp4_path = get_lone_ext_file_path_in_dir(out_parent_dir_path, ".mp4")
-    out_ttml_path = get_lone_ext_file_path_in_dir(out_parent_dir_path, "en.ttml")
-    return out_mp4_path, out_ttml_path
+    out_ttml_path = get_lone_ext_file_path_in_dir(out_parent_dir_path, ".ttml")
+    return out_mp4_path, out_ttml_path, path_safe_vid_title
 
 
 def dl_yt_vid_and_sub__as__mkv_w_embedded_sub__w_vid_title(vid_url, out_parent_dir_path, replace_spaces_with = "_", re_time_subs = True):
+    fsu.delete_if_exists(out_parent_dir_path)
+    Path(out_parent_dir_path).mkdir(parents=True, exist_ok=True)
+
     # dl with separate mp4 and srt sub file
-    mp4_path, ttml_path = dl_yt_vid_and_sub__as__mp4_and_sub__w_vid_title(vid_url, out_parent_dir_path, replace_spaces_with)
+    tmp_mp4_ttml_dl_dir_path = os.path.join(out_parent_dir_path, "tmp_mp4_ttml_dl_dir")
+    # mp4_path, ttml_path = dl_yt_vid_and_sub__as__mp4_and_sub__w_vid_title(vid_url, out_parent_dir_path, replace_spaces_with)
+    mp4_path, ttml_path, path_safe_vid_title = dl_yt_vid_and_sub__as__mp4_and_sub__w_vid_title(vid_url, tmp_mp4_ttml_dl_dir_path, replace_spaces_with)
 
     # re-time subs if needed
+    print(f"Fixing .ttml subtitle timing for {ttml_path=}...")
     if re_time_subs:
         _fix_ttml_sub_times(ttml_path)
 
     # Convert sub to srt
-    srt_sub_path = mp4_path.replace(".mp4", ".srt")
+    # srt_sub_path = mp4_path.replace(".mp4", ".srt")
+    # srt_sub_file_name = Path(mp4_path).name.replace(".mp4", ".srt")
+    # print(f"{srt_sub_file_name=}")
+    srt_sub_path = os.path.join(out_parent_dir_path, path_safe_vid_title + ".srt")
+    fsu.delete_if_exists(srt_sub_path)
+    print(f"Converting .ttml to .srt: {ttml_path=}, {srt_sub_path=}...")
     convert_subs(ttml_path, srt_sub_path)
 
     # Combine mp4 and srt to make final mkv
-    out_mkv_path = mp4_path.replace(".mp4", ".mkv")
+    print(f"Combining .mp4 with .srt to make .mkv...")
+    print(f"{mp4_path=}")
+    print(f"{srt_sub_path=}")
+
+    out_mkv_path = srt_sub_path.replace(".mp4", ".mkv")
+    print(f"{out_mkv_path=}")
+    # exit()
+    print(f"{mp4_path=}")
+    print(f"{srt_sub_path=}")
+    print(f"{out_mkv_path=}")
     combine_mp4_and_sub_into_mkv(mp4_path, srt_sub_path, out_mkv_path)
 
     # Delete old files
-    fsu.delete_if_exists(mp4_path)
-    fsu.delete_if_exists(ttml_path)
+    # fsu.delete_if_exists(mp4_path)
+    # fsu.delete_if_exists(ttml_path)
+    fsu.delete_if_exists(tmp_mp4_ttml_dl_dir_path)
     fsu.delete_if_exists(srt_sub_path)
 
+
+def _choose_best_sub_file_from_l(sub_path_l):
+    SUB_PRIORITY_PATH_L = [".en-orig", ".en."]
+
+    for sub_type_str in SUB_PRIORITY_PATH_L:
+        for sub_path in sub_path_l:
+            print(f"{Path(sub_path).name=}")
+            if sub_type_str in Path(sub_path).name:
+                return sub_path
+    print(f"WARNING - No sub type recognized, picking first: {sub_path_l=}")
+    return sub_path_l[0]
+
+def _delete_all_but_best_sub_file_in_dir(dir_path, sub_file_ext = ".ttml"):
+    sub_file_path_l = list(Path(dir_path).glob(f"*{sub_file_ext}"))
+    best_sub_file_path = _choose_best_sub_file_from_l(sub_file_path_l)
+
+    if ".en-en." in Path(best_sub_file_path).name:
+        raise(No_Valid_Subs_Exception)
+
+    for sub_file_path in sub_file_path_l:
+        if sub_file_path != best_sub_file_path:
+            fsu.delete_if_exists(sub_file_path)
 
 ####################################################################################################
 # Work from playlist_dl_dir
@@ -208,7 +267,7 @@ def make_mkv_vid_w_embedded_subs_vids_from_separate_sub_yt_playlist_dl_dir(in_pl
 
     for vid_dl_dir_path in vid_dl_dir_path_l:
         mp4_path = get_lone_ext_file_path_in_dir(vid_dl_dir_path, ".mp4")
-        ttml_path = get_lone_ext_file_path_in_dir(vid_dl_dir_path, "en.ttml")
+        ttml_path = get_lone_ext_file_path_in_dir(vid_dl_dir_path, ".ttml")
         print(f"{mp4_path=}")
 
         # out_mkv_path & tmp_srt_path
@@ -241,7 +300,9 @@ def make_mkv_vid_w_embedded_subs_vids_from_separate_sub_yt_playlist_dl_dir(in_pl
 # Could improve with threading
 # Great 3 short vid test playlist: https://www.youtube.com/playlist?list=PLfAIhxRGcgam-4wROzza_wfzdHoBJgj2J
 def dl_all_videos_in_playlist(playlist_url, out_dir_path, replace_spaces_with = "_", sub_style = "no_subs", vid_ext = "mp4"):
-    """ Fixing sub timing is outside the scope of this func """
+    """ Fixing sub timing is outside the scope of this func 
+        - Currently skips any videos that don't have compatible subs like .en-en. >:(
+    """
     # https://www.codegrepper.com/tpc/python+download+youtube+playlist
     p = Playlist(playlist_url)
 
@@ -264,12 +325,17 @@ def dl_all_videos_in_playlist(playlist_url, out_dir_path, replace_spaces_with = 
         if sub_style == "separate_file__mp4_ttml":
             dl_dir_path = os.path.join(playlist_dir_path, path_safe_video_title)
             print(f"{dl_dir_path=}")
-            dl_yt_vid_and_sub__as__mp4_and_sub__w_vid_title(video.watch_url, dl_dir_path)
+            try:
+                dl_yt_vid_and_sub__as__mp4_and_sub__w_vid_title(video.watch_url, dl_dir_path)
+            except No_Valid_Subs_Exception:
+                print(f"WARNING - Got No_Valid_Subs_Exception - Deleting any work done if needed: {dl_dir_path=}")
+                fsu.delete_if_exists(dl_dir_path)
 
         elif sub_style == "embed_subs_as_mp4":
             out_template = os.path.join(playlist_dir_path, path_safe_video_title) + ".%(ext)s"
 
-            cmd = f'yt-dlp -f bestvideo[ext={vid_ext}]+bestaudio[ext=ttml]/best[ext={vid_ext}]/best --write-auto-subs --sub-lang "en.*" --embed-subs --no-playlist -o "{out_template}" {video.watch_url}'
+            # cmd = f'yt-dlp -f bestvideo[ext={vid_ext}]+bestaudio[ext=ttml]/best[ext={vid_ext}]/best --write-auto-subs --sub-lang ".en.*" --embed-subs --no-playlist -o "{out_template}" {video.watch_url}'
+            cmd = f'yt-dlp -f bestvideo[ext={vid_ext}]+bestaudio[ext=ttml]/best[ext={vid_ext}]/best --write-auto-subs --sub-lang en,en-us,en_us,en-US,en_US --embed-subs --no-playlist -o "{out_template}" {video.watch_url}'
             print(f"Running cmd: {cmd}...")
             subprocess.call(cmd, shell = True)
 
@@ -310,14 +376,29 @@ def dl_yt_playlist__fix_sub_times_convert_to_mkvs_w_embedded_subs(playlist_url, 
 
 
 if __name__ == "__main__":
-    # # Great 3 short vid test playlist: https://www.youtube.com/playlist?list=PLfAIhxRGcgam-4wROzza_wfzdHoBJgj2J
-    # dl_yt_playlist__fix_sub_times_convert_to_mkvs_w_embedded_subs("https://www.youtube.com/playlist?list=PLfAIhxRGcgam-4wROzza_wfzdHoBJgj2J",
-    dl_yt_playlist__fix_sub_times_convert_to_mkvs_w_embedded_subs("https://www.youtube.com/playlist?list=PLJBo3iyb1U0eNNN4Dij3N-d0rCJpMyAKQ",
-     out_dir_path = "C:/Users/Brandon/Documents/Personal_Projects/youtube_utils/ignore/FG_TBS_pl_mkv_e_subs_re_timed")
+    # # # Great 3 short vid test playlist: https://www.youtube.com/playlist?list=PLfAIhxRGcgam-4wROzza_wfzdHoBJgj2J
+    # # dl_yt_playlist__fix_sub_times_convert_to_mkvs_w_embedded_subs("https://www.youtube.com/playlist?list=PLfAIhxRGcgam-4wROzza_wfzdHoBJgj2J",
+    # dl_yt_playlist__fix_sub_times_convert_to_mkvs_w_embedded_subs("https://www.youtube.com/playlist?list=PLJBo3iyb1U0eNNN4Dij3N-d0rCJpMyAKQ",
+    #  out_dir_path = "C:/Users/Brandon/Documents/Personal_Projects/youtube_utils/ignore/FG_TBS_pl_mkv_e_subs_re_timed")
 
-    # dl_yt_vid_and_sub__as__mkv_w_embedded_sub__w_vid_title(vid_url = "https://www.youtube.com/watch?v=ORAymXqGREY&list=PLJBo3iyb1U0eNNN4Dij3N-d0rCJpMyAKQ&index=3",
-    #  out_parent_dir_path = "C:/Users/Brandon/Documents/Personal_Projects/youtube_utils/ignore/INIV2_mkv_yt_dl_test",
-    #   replace_spaces_with = "_")
+    # out_template = "C:/Users/Brandon/Documents/Personal_Projects/youtube_utils/ignore/test_sub" + ".%(ext)s"
+    # # url = "https://www.youtube.com/watch?v=gT1bqKc6zKo&list=PLfAIhxRGcgam-4wROzza_wfzdHoBJgj2J&index=2"
+    # url = "https://www.youtube.com/watch?v=ORAymXqGREY&list=PLJBo3iyb1U0eNNN4Dij3N-d0rCJpMyAKQ&index=3"
+    # cmd = 'yt-dlp -f bestvideo[ext=mp4]+bestaudio[ext=ttml]/best[ext=mp4]/best  --default-search "ytsearch"  --write-auto-subs --sub-lang "en.*" --sub-format ttml --no-playlist -o "{out_template}" {url}'
+    # resp = subprocess.Popen(cmd, stdout = subprocess.PIPE, shell = False)
+    # print(f"{resp=}")
+    # for line in iter(p.stdout.readline, b''):
+            
+    #     # if decode:
+    #     line = line.decode("utf-8") 
+    #     print(line)
+
+    # dl_yt_vid_and_sub__as__mp4_and_sub__w_vid_title(vid_url, out_parent_dir_path, replace_spaces_with = "_")
+
+    # dl_yt_vid_and_sub__as__mkv_w_embedded_sub__w_vid_title(vid_url = "https://www.youtube.com/watch?v=yPM77NPZyJo&list=PLJBo3iyb1U0eNNN4Dij3N-d0rCJpMyAKQ&index=26",
+    dl_yt_vid_and_sub__as__mkv_w_embedded_sub__w_vid_title(vid_url = "https://www.youtube.com/watch?v=ORAymXqGREY&list=PLJBo3iyb1U0eNNN4Dij3N-d0rCJpMyAKQ&index=3",
+     out_parent_dir_path = "C:/Users/Brandon/Documents/Personal_Projects/youtube_utils/ignore/INIV3_mkv_yt_dl_test",
+      replace_spaces_with = "_")
 
     # convert_subs("C:/Users/Brandon/Documents/Personal_Projects/youtube_utils/ignore/INIV_mkv_yt_dl_test/Invention_that_backfires_2.en.ttml",
     # "C:/Users/Brandon/Documents/Personal_Projects/youtube_utils/ignore/INIV_mkv_yt_dl_test/Invention_that_backfires_2.srt")
